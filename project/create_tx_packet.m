c4f898b79d0194c0f9c589b019e1bd6387910f26
nscwong <nsc.wong@ucla.edu>
2017-02-15 13:17:04 -0800

    Added to packet creation and plots for project phase 1.

--- project/create_tx_packet.m
+++ project/create_tx_packet.m
@@ -1,4 +1,4 @@
-function tx_packet = create_tx_packet()
+function tx_packet = create_tx_packet(data)
 % Creates a TX Packet Structure according to the EWC standard
 % 
 % Ignore: Encoder Parser & FEC Encoder
@@ -6,9 +6,31 @@ function tx_packet = create_tx_packet()
 
 % Create the subframes
 subframe_lstf = sqrt(0.5)*[0,0,1+1j,0,0,0,-1-1j,0,0,0,1+1j,0,0,0,-1-1j,0,0,0,-1-1j,0,0,0,1+1j,0,0,0,0,0,0,0,-1-1j,0,0,0,-1-1j,0,0,0,1+1j,0,0,0,1+1j,0,0,0,1+1j,0,0,0,1+1j,0,0];
-subframe_htltf1 = [];
-subframe_htsig = [];
+% Page 26 and 27 of spec
+subframe_htltf1 = [1,1,1,1,-1,-1,1,1,-1,1,-1,1,1,1,1,1,1,-1,-1,1,1,-1,1,-1,1,1,1,1,0,1,-1,-1,1,1,-1,1,-1,1,-1,-1,-1,-1,-1,1,1,-1,-1,1,-1,1,-1,1,1,1,1,-1,-1];
+% Page 21 and 22 of spec
+mcs = zeros(1,7);       % Modulation and Coding Scheme Table 1 LSB First
+bw2040 = 0;             % 0 if 20MHz or 40MHz upper/lower, 1 if 40MHz
+length = zeros(1,16);   % The number of bytes in frame - LSB first
+aggregation = 0;        % 1 if PPDU in data packet contains an A-MPDU
+stbc = zeros(1,2);      %
+advanced_coding = 0;    % 1 if Advanced Coding, 0 if BCC
+short_gi = 0;           % Indicate that short GI is used after HT training
+nhtltf = zeros(1,2);    % Number of HT-LTF
+crc = zeros(1,7);       % CRC bits 
+tail_bits = zeros(1,6); % Tail bits
+subframe_htsig = [mcs,bw2040,length,1,1,1,aggregation,stbc,advanced_coding,short_gi,nhtltf,crc,tail_bits];
+% Page 27 and 28 of spec
+P_htltf = [ ...     % Polarity matrix
+     1 -1  1  1;    % P_htlf(i,n) represents polarity of the
+     1  1 -1  1;    % ith spatial stream in the
+     1  1  1 -1;    % nth HT training symbol
+    -1  1  1  1;
+];
 subframe_htltfs = [];
-subframe_data = [];
+% Page 28 and 29 of spec
+subframe_data = data;
+
+
 
 end
--- project/ee230b_project_phase1.m
+++ project/ee230b_project_phase1.m
@@ -12,8 +12,105 @@
 %  No coding (convolutional or LDPC) will be included in our study
 
 %% Initialization Parameters
+f_c = 2.4e9;            % Carrier Frequency for 802.11
+bandwidth = 20e6;       % 
+d = 140;                % Distance between TX and RX (m)
+nbits = 20;             % Data length in symbols
+SNR_dB = 2;             % USER-SPECIFIED SNR
 
+% Modulation Parameters
+rrc_beta = 0;
+rrc_nsym = 8;
+rrc_sampspersym = 4;
 
+% Channel Parameters
+alpha = 3;              % Variable for path gain
 
 %% Simulation
 
+% MODULATION
+Payload = randi([0 1], nbits, 1);
+Payload_syms = 2*Payload - 1; % For testing purposes
+
+rctFilt = comm.RaisedCosineTransmitFilter(...
+  'Shape',                  'Square root', ...
+  'RolloffFactor',          rrc_beta, ...
+  'FilterSpanInSymbols',    rrc_nsym, ...
+  'OutputSamplesPerSymbol', rrc_sampspersym);
+%fvtool(rctFilt, 'Analysis', 'impulse')
+
+TX = step(rctFilt, [Payload_syms; zeros(rrc_nsym/2,1)]);
+
+% CHANNEL
+
+% Path Gain
+lambda = 3e8/f_c;
+Path_Gain = (lambda/(4*pi))^2 * (1/d)^alpha;
+
+% Shadow Fading
+Shadow_dB = normrnd(0, 1, numel(TX), 1);
+Shadow = 10.^(Shadow_dB/10);
+
+if nbits > 10e3
+figure; % PDF of log-normal shadow fading
+histogram(Shadow, 'Normalization', 'pdf');
+title('Shadow Fading Channel Model: Amplitude');
+xlabel('Amplitude of Shadow');
+ylabel('Probability Density Function');
+
+figure; % PDF of log-normal shadow fading
+histogram(10*log10(Shadow), 'Normalization', 'pdf');
+title('Shadow Fading Channel Model: Amplitude');
+xlabel('Amplitude of Log-Normal Shadow');
+ylabel('Probability Density Function');
+end
+
+% Rayleigh Fading
+ComplexFading = normrnd(0, 1, numel(TX), 1) + 1i.*normrnd(0, 1, numel(TX), 1);
+ComplexFading_abs = abs(ComplexFading);
+ComplexFading_ang = atan2(imag(ComplexFading), real(ComplexFading));
+
+if nbits > 10e3
+figure; % PDF of fast fading amplitude
+histogram(ComplexFading_abs, 'Normalization', 'pdf');
+title('Fast Fading Channel Model: Amplitude');
+xlabel('Amplitude of Complex Gaussian');
+ylabel('Probability Density Function');
+
+figure; % PDF of fast fading phase
+histogram(ComplexFading_ang, (-pi):(pi/48):(pi), 'Normalization', 'pdf');
+title('Fast Fading Channel Model: Phase');
+xlabel('Phase of Complex Gaussian (radians)');
+ylabel('Probability Density Function');
+end
+
+RX_1 = TX;
+RX_2 = RX_1*sqrt(Path_Gain);
+RX_3 = RX_2.*sqrt(Shadow);
+RX_4 = RX_3.*sqrt(abs(ComplexFading));
+
+% AWGN Noise
+Signal = RX_4;
+SNR = 10^(SNR_dB/10);                       % Convert to linear
+Esym = sum(abs(Signal).^2)/numel(Signal);   % Actual symbol energy
+N0 = Esym/SNR;
+if (isreal(Signal))
+    noise_sigma = sqrt(N0);
+    Noise = noise_sigma*randn(numel(Signal),1);
+else
+    noise_sigma = sqrt(N0/2);
+    Noise = noise_sigma*(randn(numel(Signal),1) + 1j*randn(numel(Signal),1));
+end
+
+RX_5 = RX_4 + Noise;
+
+if nbits <= 50
+figure;
+stem(RX_2);
+hold on
+stem(RX_3);
+stem(RX_4);
+stem(RX_5);
+hold off
+legend('Path Gain', '+ Shadow Fading', '+ Fast Fading', '+ Noise');
+end
