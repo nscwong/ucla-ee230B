9f94691583d81d44e6dbfd2bcb3c51ae48ea0fda
gkhadge <gkhadge@g.hmc.edu>
2017-03-08 21:08:24 -0800

    Fixed your sinc interpolation code. The convolution has an offset, since the convolution outputs twice as many data points. You didn't re-center properly. Also, fixed the number of taps to 201 here. I think your sinc argument might have been incorrect too.

--- project/sinc_reconstruction.m
+++ project/sinc_reconstruction.m
@@ -1,4 +1,5 @@
-
+close all
+clear
 % sinc reconstruction
 
 Ts = 0.1;
@@ -7,11 +8,27 @@ n = 0:1:40/Ts;
 nTs = n*Ts;
 y = sin(n*Ts);
 
-tau = 1;
-yprime = conv(y,sinc(tau + nTs));
+% Filter is 201 taps total in this config
+numTaps = 100;
+sincTaps = -numTaps:numTaps+1;
 
-figure;
-plot(n,y,'-o');
+% tau_t = 0; % Tau_t is in time
+% tau_s = tau_t/Ts; % Tau_s is in samples
+tau_s = -5;
+yprime = conv(y,sinc(tau_s + sincTaps),'same');
+
+% tau = 1.5;
+% yprime = sinc_interp(n',y,n'+tau);
+
+figure(1);
+plot(n,y,'r-o');
 hold on
-plot(yprime(1:numel(nTs)),'-*');
+plot(n,yprime,'b-*');
+ylim([-11,11])
 hold off
+
+% figure(2)
+% plot(sinc(tau + sincTaps),'.-')
+
+
+
