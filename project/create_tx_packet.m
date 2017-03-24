31e530dc117c3f9824966ebb583fa536dd54c038
gkhadge <gkhadge@g.hmc.edu>
2017-03-23 18:14:44 -0700

    Added data modulation. Still need to test and build demodulator.

--- project/create_tx_packet.m
+++ project/create_tx_packet.m
@@ -11,6 +11,22 @@ function tx_packets = create_tx_packet(data, nss)
 
 tx_packets = cell(nss,1);
 
+if nss == 1
+    psi = [ 1, 1, 1,-1];
+elseif nss == 2
+    psi = [ 1, 1,-1,-1;
+            1,-1,-1, 1];    
+elseif nss == 3
+    psi = [ 1, 1,-1,-1;
+            1,-1, 1,-1;
+           -1, 1, 1,-1];    
+else % nss ==4
+    psi = [ 1, 1, 1,-1;
+            1, 1,-1, 1;
+            1,-1, 1, 1;
+           -1, 1, 1, 1];   
+end
+
 % Initial parameters -- copied for convenience
 f_c = 2.4e9;            % Carrier Frequency for 802.11
 bandwidth = 20e6;       % Bandwidth
@@ -19,7 +35,7 @@ df = 312.5e3;           % Subcarrier frequency spacing (delta f)
 nbits = 4;              % Data length in symbols
 
 M = 4;      % M-QAM number of modulation points
-data = randi([0 1], 1, 9);  % 1xn vector of data
+% data = randi([0 1], 1, 9);  % 1xn vector of data
 
 nfft = 64;
 
@@ -36,6 +52,8 @@ T_lstf = 8e-6;
 T_htltf1 = 8e-6;
 T_htltfs = 4e-6;
 T_GI = 0.8e-6;
+T_sym = 4e-6;
+N_dbps = NSD_ht*log2(M);    % Number of databits/symbol
 
 time = 0:dt:(T_lstf+T_htltf1+3*T_htltfs)-dt; % Time vector for Preamble
 
@@ -121,19 +139,82 @@ for iss = 1:nss
     tx_packets{iss} = [subframe_lstf,subframe_htltf1,subframe_htltf2,subframe_htltf3,subframe_htltf4];
 end
 % Data
-padamt = M - mod(numel(data),M);    % Number of zeros to pad
-data = [data zeros(1,padamt)];      % Pad to fit modulation scheme
-data_i = reshape(data,log2(M),numel(data)/log2(M));    % Change to int
+if M == 4
+    K_mod = 1/sqrt(2);
+elseif M==16
+    K_mod = 1/sqrt(10);
+elseif M==64
+    K_mod = 1/sqrt(42);
+else
+    K_mod = 1;
+end
+    
+N_syms = ceil(numel(data)/N_dbps);
+
+padamt = N_syms*N_dbps - numel(data);    % Number of zeros to pad
+data_bits = [data, zeros(1,padamt)];      % Pad to fit modulation scheme
+data_i = reshape(data_bits,log2(M),numel(data_bits)/log2(M));    % Change to int
 data_i = (2.^((log2(M)-1):-1:0))*data_i;
-data_mod = qammod(data_i,M);  % Modulated data
+data_mod = K_mod*qammod(data_i,M);  % Modulated data {-3,-1,1,3}
+
+% Add data to signal
+% DATA -- Page 35 of spec
+iss = 1;
+for n = 0:N_syms-1
+    
+    block_indx = NSD_ht*n + 1;
+    
+    t_sym = 0:dt:(T_sym-dt);
+    data_sym = zeros(size(t_sym));
+    % add data into signal
+    for k = 0:NSD_ht-1
+        data_sym = data_sym + data_mod(block_indx+k)*...
+                          exp(2j*pi*M_of_k(k)*df*(t_sym-T_GI-T_iTX_CS_ht(nss,iss)));
+    end
+    % add pilots into signal
+    pilot_ks = [-21,-7,7,21];
+    for ind = 1:length(pilot_ks)
+        data_sym = data_sym + psi(iss,1+mod(n+ind-1,4))*...
+                          exp(2j*pi*pilot_ks(ind)*df*(t_sym-T_GI-T_iTX_CS_ht(nss,iss)));
+    end
+    data_sym = data_sym/sqrt(Ntone_htdata);
+    
+    % Append data symbol to i-th spatial stream
+    tx_packets{iss} = horzcat(tx_packets{iss},data_sym);
+    
+    % Increment spatial stream
+    iss = mod(iss+1,nss);
+end
+
 
 % FIXME:  Fix Data Packet
 
 % Final Packet
 
 % Plot first signal
-figure;
-plot(time, real(tx_packets{1}));
-title('create\_tx\_packet');
+% figure;
+% plot(time, real(tx_packets{1}));
+% title('create\_tx\_packet');
 
+end
+
+% helper function for sorting data into proper subcarriers
+function out=M_of_k(k)
+    if k < 0
+        out = NaN; % Invalid k
+    elseif k <= 6
+        out = k-28;
+    elseif k <=19
+        out = k-27;
+    elseif k <= 25
+        out = k-26;
+    elseif k <= 31
+        out = k-25;
+    elseif k <= 44
+        out = k-24;
+    elseif k <= 51
+        out = k-23;
+    else        
+        out = NaN; % Invalid k
+    end
 end
